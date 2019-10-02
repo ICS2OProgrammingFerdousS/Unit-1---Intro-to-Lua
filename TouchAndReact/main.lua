4a54fb3d8d03b0dbae47cef11b0f416f68f556f0
s457232 <55598449+s457232@users.noreply.github.com>
2019-10-02 14:07:36 -0400

    added touch and react

--- TouchAndReact/main.lua
+++ TouchAndReact/main.lua
@@ -6,3 +6,13 @@
 
 -- Your code here
 
+display.setDefault("background", 1, 1,0.7)
+
+local firstButton= display.newImageRect("Images/ Fast Button Active@2x.png", 197, 96)
+firstButton.x= 500
+firstButton.y= 400
+firstButton.isVisible= true
+
+-- creating second button
+local  secondButton= display.newImageRect("Images/ Fast Button Inactive@2x.png", 197, 96)
+--secondButton
